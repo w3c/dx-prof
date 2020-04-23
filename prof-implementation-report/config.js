8a5cc1676ed264e5432bbf096ef86ecc493c98f4
Nicholas Car <nicholas.car@surroundaustralia.com>
2020-04-23 21:10:44 +1000

    first draft of Impl Rpt

---
+++ prof-implementation-report/config.js
@@ -0,0 +1,40 @@
+var respecConfig = {
+    specStatus: "base",
+    shortName: "dx-prof-implementation-report",
+    edDraftURI: "https://w3c.github.io/dxwg/dx-prof/prof-implementation-report",
+    canonicalURI: "TR",
+    testSuiteURI: "https://github.com/w3c/dx-prof/testsuite/",
+    editors: [
+        {
+            name:       "Rob Atkinson",
+            mailto:     "ratkinson@ogc.org",
+            company:    "Metalinkage, Open Geospatial Consortium",
+            companyURL: "http://www.opengeospatial.org/",
+            orcid:      "0000-0002-7878-2693"
+        },
+        {
+            name:       "Nicholas J. Car",
+            mailto:     "nicholas.car@surroundaustralia.com",
+            company:    "SURROUND Australia Pty Ltd and before at CSIRO",
+            companyURL: "https://surroundaustralia.com",
+            orcid:      "0000-0002-8742-7730",
+            w3cid:      70131
+        }],
+    wg: "Dataset Exchange Working Group",
+    wgURI: "https://www.w3.org/2017/dxwg/",
+    processVersion: 2020,
+    localBiblio: {
+        "PROF-GUIDANCE": {
+            editors: [
+                "Rob Atkinson",
+                "Karen Coyle",
+                "Antoine Isaac",
+                "Nicholas J. Car"
+            ],
+            href: "https://w3c.github.io/dxwg/profiles/",
+            title: "Profile Guidance",
+            date: " 2019-04-24",
+            status: "W3C Editor's Draft"
+        },
+    }
+};
