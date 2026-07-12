db308de764f4a023fdb7b5c724417965cc5476cb
YoucTagh <youc.learn.13@gmail.com>
2026-07-12 20:53:52 +0200

    fix respec error and prepare for rec track

--- prof/config.js
+++ prof/config.js
@@ -19,6 +19,7 @@ var respecConfig = {
         company:    "Open Geospatial Consortium",
         companyURL: "http://www.opengeospatial.org/",
         orcid:      "0000-0002-7878-2693",
+        w3cid:      90763
       },
       {
         name:       "Les Kneebone",
