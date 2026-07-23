e0489e9f605efab6626bba12a4464d6457bc29fb
Nicholas Car <nick@kurrawong.ai>
2026-07-23 22:47:08 +1000

    dx-prof -> dx-prof-1.0

--- prof/config.js
+++ prof/config.js
@@ -1,11 +1,11 @@
 var respecConfig = {
     specStatus: "FPWD",
-    shortName: "dx-prof",
-    edDraftURI: "https://www.w3.org/TR/2026/WD-dx-prof-20260730/",
-    prevRecURI: "https://www.w3.org/TR/2019/NOTE-dx-prof-20191218/",
+    shortName: "dx-prof-1.0",
+    edDraftURI: "https://www.w3.org/TR/2026/WD-dx-prof-1.0-20260730/",
+    prevRecURI: "https://www.w3.org/TR/2019/NOTE-dx-prof-1.0-20191218/",
     canonicalURI: "TR",
     copyrightStart: "2026",
-    implementationReportURI: "https://w3c.github.io/dx-prof/prof-impl-rpt/index.html",
+    implementationReportURI: "https://w3c.github.io/dx-prof-1.0/prof-impl-rpt/index.html",
     editors: [
       {
         name:       "Nicholas J. Car",
