68efdff7383fe14d07bae118f9f8f92fdbcb1b56
Nicholas Car <nick@kurrawong.ai>
2026-07-23 22:39:53 +1000

    add all the ancillary files

---
+++ publication-snapshots/WD-dx-prof-20260730/config.js
@@ -0,0 +1,116 @@
+var respecConfig = {
+    specStatus: "FPWD",
+    shortName: "dx-prof",
+    edDraftURI: "https://www.w3.org/TR/2026/WD-dx-prof-20260730/",
+    prevRecURI: "https://www.w3.org/TR/2019/NOTE-dx-prof-20191218/",
+    canonicalURI: "TR",
+    copyrightStart: "2026",
+    implementationReportURI: "https://w3c.github.io/dx-prof/prof-impl-rpt/index.html",
+    editors: [
+      {
+        name:       "Nicholas J. Car",
+        mailto:     "nick@kurrawong.ai",
+        company:    "KurrawongAI",
+        companyURL: "https://kurrawong.ai",
+        orcid:      "0000-0002-8742-7730",
+        w3cid:      70131
+      },
+      {
+        name:       "Rob Atkinson",
+        mailto:     "ratkinson@ogc.org",
+        company:    "Open Geospatial Consortium",
+        companyURL: "http://www.opengeospatial.org/",
+        orcid:      "0000-0002-7878-2693",
+        w3cid:      90763
+      },
+      {
+        name:       "Les Kneebone",
+        mailto:     "les@kurrawong.ai",
+        company:    "KurrawongAI",
+        companyURL: "https://kurrawong.ai",
+        orcid:      "0000-0001-5640-3202",
+        w3cid:      93930
+      },
+      {
+        name: "Yousouf Taghzouti",
+        company: "Inria",
+        companyURL: "https://www.inria.fr/",
+        mailto: "yousouf.taghzouti@inria.fr",
+        orcid: "0000-0003-4509-9537",
+        w3cid: 143054
+      }
+    ],
+    group: "wg/dx",
+    wg: "Dataset Exchange Working Group",
+    wgURI: "https://www.w3.org/groups/wg/dx/",
+    wgPublicList: "public-dxwg-comments",
+    wgPatentURI: "https://www.w3.org/2004/01/pp-impl/99375/status",
+    inlineCSS: "true",
+    lint: {"no-unused-dfns": false},
+    issueBase: "https://github.com/w3c/dx-prof/issues/",
+    github:    "w3c/dx-prof/",
+    localBiblio: {
+        PDF: {
+            href: "https://www.iso.org/standard/51502.html",
+            title: "Document management -- Portable document format -- Part 1: PDF 1.7",
+            date: "2008-07",
+            status: "ISO Standard"
+        },
+        SHEX: {
+            href: "https://shexspec.github.io/spec/",
+            title: "Shape Expressions Language 2.next",
+            date: "2019-08-31",
+            status: "W3C Community Group Draft Report"
+        },
+        "DX-PROF-GUIDANCE": {
+            editors: [
+                "Rob Atkinson",
+                "Karen Coyle",
+                "Antoine Isaac",
+                "Nicholas J. Car"
+            ],
+            href: "https://w3c.github.io/dxwg/profiles/",
+            title: "Profile Guidance",
+            date: "2018-12-31",
+            status: "W3C Editor's Draft"
+        },
+        "DX-PROF-IETF": {
+            authors: [
+                "L. Svensson",
+                "R. Verborgh",
+                "H. Van de Sompel"
+            ],
+            href: "https://profilenegotiation.github.io/I-D-Profile-Negotiation/I-D-Profile-Negotiation",
+            title: "Indicating and Negotiating Profiles in HTTP",
+            date: "2019-11-20",
+            status: "IETF Internet-Draft"
+        },
+        SCHEMATRON: {
+            href: "https://www.iso.org/standard/55982.html",
+            title: "ISO/IEC 19757-3:2016 Information technology -- Document Schema Definition Languages (DSDL) -- Part 3: Rule-based validation -- Schematron",
+            date: "2016-01",
+            status: "ISO Standard"
+        },
+        "MODSPEC": {
+          editors: [
+            "OGC Policy SWG"
+          ],
+          href: "https://www.ogc.org/standards/modspec/" ,
+          title: "The ModSpec Model - Part 1: Core - A Standard for Designing and Writing Modular Standards" ,
+          status: "Open Geospatial Consortium Implementation Standard" ,
+          date: "2026-05-21"
+        },
+        "CFG-MGMT": {
+          authors: [
+            "Serena Coetzee",
+            "Simon Cox",
+            "John Herring"
+          ],
+          href: "https://doi.org/10.1109/SIIT.2011.6083604" ,
+          title: "Configuration management of a system of interdependent standards" ,
+          publisher: "IEEE" ,
+          status: "7th International Conference on Standardization and Innovation in Information Technology (SIIT), Berlin, 2011, pp. 1-12" ,
+          date: "2011-11-21"
+        }
+    }
+};
